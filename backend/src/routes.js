5ce9b4ad0b87005ecbebf39f11f45e21f0d34224
Raphael Rezzieri <rezzieri@gmail.com>
2020-03-24 20:10:42 -0400

    refactor project - Controllers

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,21 +1,8 @@
 const express = require('express');
-const crypto = require('crypto');
-const connection = require('./database/connection');
-
+const OngController = require('./controllers/OngController');
 const routes = express.Router();
 
-routes.get('/ong', async (request, response) => {
-    const ongs = await connection('ong').select('*');
-    return response.json({ ongs });
-});
-
-routes.post('/ong', async (request, response) => {
-    const { name, email, whatsapp, city, uf } = request.body;
-    const id = crypto.randomBytes(4).toString('HEX');
-    await connection('ong').insert({
-        id, name, email, whatsapp, city, uf
-    });
-    return response.json({ id });
-});
+routes.get('/ong', OngController.index);
+routes.post('/ong', OngController.create);
 
 module.exports = routes;
