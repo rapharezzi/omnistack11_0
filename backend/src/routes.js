4d4581f407a2b48aeb7f3d3b875600e72f5a35c0
Raphael Rezzieri <rezzieri@gmail.com>
2020-03-24 21:15:50 -0400

    SessionController - Login

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
+
 const routes = express.Router();
+
+routes.post('/session', SessionController.create);
 
 routes.get('/ong', OngController.index);
 routes.post('/ong', OngController.create);
+
+routes.get('/profile', ProfileController.index);
 
 routes.post('/incident', IncidentController.create);
 routes.get('/incident', IncidentController.index);
