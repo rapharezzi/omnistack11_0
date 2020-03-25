69a1fd67a1fb5b44bb0e7d41341745c9ec4ac2cc
Raphael Rezzieri <rezzieri@gmail.com>
2020-03-24 20:58:27 -0400

    Incident Controller - insert, delete and list

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
 const routes = express.Router();
 
 routes.get('/ong', OngController.index);
 routes.post('/ong', OngController.create);
+
+routes.post('/incident', IncidentController.create);
+routes.get('/incident', IncidentController.index);
+routes.delete('/incident/:id', IncidentController.delete);
 
 module.exports = routes;
